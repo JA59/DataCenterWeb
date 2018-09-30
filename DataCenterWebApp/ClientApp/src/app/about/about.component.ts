8c7cca957e7d614f8d8d8058a451b122c92d388c
ja59 <joeamato59@outlook.com>
2018-09-30 08:38:26 -0400

    slide changes

--- DataCenterWebApp/ClientApp/src/app/about/about.component.ts
+++ DataCenterWebApp/ClientApp/src/app/about/about.component.ts
@@ -7,7 +7,7 @@ import { Component } from "@angular/core";
 })
 
 export class AboutComponent {
-  title = "ASP.NET Core and Angular";
+  title = "Implementing a web based solution with ASP.NET Core and Angular";
   slideNumber: number = 1;
 
   constructor() {
